55f6806f4bd2080a687f1584768d6dffed4f79ad
Aashvi99 <aashvi99@gmail.com>
2026-02-22 13:06:07 -0500

    Sync backend with EC2 production version

---
+++ backend/smart_study_planner/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+urlpatterns = [
+    path('', lambda request: HttpResponse("Smart Study Planner Backend is Running 🚀")),
+    path('admin/', admin.site.urls),
+    path('api/', include('planner.urls')),
+]
