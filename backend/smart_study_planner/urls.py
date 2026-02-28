4bfa6c170a062923be5f0fa798509af86bdb8cf4
Aashvi99 <102274598+Aashvi99@users.noreply.github.com>
2026-02-28 15:46:15 -0500

    Update urls.py

--- backend/smart_study_planner/urls.py
+++ backend/smart_study_planner/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 urlpatterns = [
-    path('', lambda request: HttpResponse("Smart Study Planner Backend is Running 🚀")),
+    path('', lambda request: HttpResponse("Smart Study Planner Backend is Running ")),
     path('admin/', admin.site.urls),
     path('api/', include('planner.urls')),
 ]
