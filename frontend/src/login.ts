78743a601e03f52dfb20528e27c57851b118dcd8
Colin Christy <colinchristy15@gmail.com>
2026-03-14 22:23:26 -0400

    Added basic checking of username and password

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.querySelector('#button-login');
-    loginButton?.addEventListener('click', () => {
-        window.location.href = 'index.html?authenticated=true';
-    });
+    loginButton?.addEventListener('click', onLoginButtonPressed);
 })
+
+function onLoginButtonPressed() {
+    const inputUsername = document.querySelector('#input-username') as HTMLInputElement;
+    const inputPassword = document.querySelector('#input-password') as HTMLInputElement;
+
+    const username = inputUsername.value;
+    const password = inputPassword.value;
+
+    if (validLogin(username, password)) window.location.href = 'index.html?authenticated=true';
+    else alert('Your username or password is incorrect.');
+}
+
+function validLogin (username: string, password: string): boolean {
+    return username != '' && password != '';
+}
